ac44a127d38b93a3835a856002d8fb725f145df8
Natalia <natalia.kowalik2@gmail.com>
2016-11-08 01:11:45 +0100

    Proste podstrony: ankieta i historia chorób

---
+++ WcfService/MediApp/Controllers/DiseasesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediApp.Controllers
+{
+    public class DiseasesController : Controller
+    {
+        // GET: Diseases
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Survey()
+        {
+            ViewBag.Message = "Wstępna diagnoza chorób";
+            return View();
+        }
+        public ActionResult DiseasesHistory()
+        {
+            ViewBag.Message = "Historia przebytych chorób";
+            return View();
+        }
+    }
+}
